a8d18e2ac8888092dd5868d9c225761ef42fcd4b
BezzB <118984781+BezzB@users.noreply.github.com>
2023-03-21 19:53:54 +0300

    Added the dynamic js

--- index.js
+++ index.js
@@ -2,52 +2,52 @@
 const artists = [
     {
         name: "John Doe",
-        portraitUrl: "https://example.com/artist1.jpg",
+        portraitUrl: src="../images/speaker1.png",
         description: "John Doe is a talented artist who specializes in abstract paintings."
       },
       {
         name: "Jane Smith",
-        portraitUrl: "https://example.com/artist2.jpg",
+        portraitUrl: "../images/speaker2.png",
         description: "Jane Smith is a contemporary artist who creates immersive installations."
       },
       {
         name: "Bob Johnson",
-        portraitUrl: "https://example.com/artist3.jpg",
+        portraitUrl: "../images/speaker3.png",
         description: "Bob Johnson is a mixed media artist who explores themes of identity and memory."
       },
       {
         name: "Sarah Lee",
-        portraitUrl: "https://example.com/artist4.jpg",
+        portraitUrl: "../images/speaker4.png",
         description: "Sarah Lee is a painter who captures the beauty of nature in her work."
       },
       {
         name: "David Kim",
-        portraitUrl: "https://example.com/artist5.jpg",
+        portraitUrl: "../images/speaker5.png",
         description: "David Kim is a sculptor who transforms everyday objects into works of art."
       },
       {
         name: "Emily Wang",
-        portraitUrl: "https://example.com/artist6.jpg",
+        portraitUrl: "../images/speaker6.png",
         description: "Emily Wang is a performance artist who explores the body in space."
       },
       {
         name: "Alex Chen",
-        portraitUrl: "https://example.com/artist7.jpg",
+        portraitUrl: "../images/speaker7.png",
         description: "Alex Chen is a photographer who captures the beauty of the human form."
       },
       {
         name: "Grace Liu",
-        portraitUrl: "https://example.com/artist8.jpg",
+        portraitUrl: "../images/speaker8.png",
         description: "Grace Liu is a digital artist who creates immersive virtual environments."
       },
       {
         name: "Mark Davis",
-        portraitUrl: "https://example.com/artist9.jpg",
+        portraitUrl: "../images/speaker9.png",
         description: "Mark Davis is a street artist who brings color and vibrancy to urban spaces."
       },
       {
         name: "Lisa Kim",
-        portraitUrl: "https://example.com/artist10.jpg",
+        portraitUrl: "../images/speaker10.png",
         description: "Lisa Kim is a graphic designer who creates striking visual identities."
       }
     // Add more artists here
